17ba8fe8475c472d8116f9312392c9cbcdc21a4b
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-15 17:32:08 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -68,7 +68,7 @@ class TicTacToe
   def won?
     WIN_COMBINATIONS.each_with_index do |element, index|
       if position_taken?(element[0]) == true
-        if @board[element[0]] == board[element[1]] && board[element[1]] == board[element[2]]
+        if @board[element[0]] == @board[element[1]] && @board[element[1]] == @board[element[2]]
           return WIN_COMBINATIONS[index]
         end
       end
