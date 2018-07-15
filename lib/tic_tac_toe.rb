0c2d975a11cadbde23cd3b51d2332b72e508e5a3
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-15 17:08:15 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -4,6 +4,17 @@ class TicTacToe
     @board = board || Array.new(9, " ")
   end
 
+  WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5],  # Middle row
+  [6,7,8], #Bottom row
+  [0,3,6],  # First column
+  [1,4,7],  #Second column
+  [2,5,8],  #Third column
+  [0,4,8],  #diagonal 1
+  [2,4,6]  #diagonal 2
+]
+
   def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
